7dfe69248941c970712305f9dfac575a0cc38e80
Jhofre2102 <jhonospinaduran@gmail.com>
2025-04-26 07:39:48 -0500

    Clase 25 Abril

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/")
+def home ():
+    return '''
+        Aplicación web con flask-afsr
+        <h1>Calcu1adora</h1>
+        <h3>Opciones Disponibles</h3>
+        <ul>
+        <li><a href="
+        
+
+
+
+
+
+'''
+
+
+

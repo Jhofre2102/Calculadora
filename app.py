667663960f4aa38b768b4005256f4e5709fc85a6
Jhofre2102 <jhonospinaduran@gmail.com>
2025-04-30 20:07:15 -0500

    Clase 30 abril

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,request,render_template
+from livereload import Server 
+from operaciones import sumar
+from operaciones import division_piso
+
+app= Flask(__name__)
+
+
+@app.route("/")
+def inicio():
+    return render_template("inicio.html")
+
+
+@app.route("/suma")
+def ruta_suma():
+    numero1=request.args.get("numero1",type=float)
+    numero2=request.args.get("numero2",type=float)
+    if numero1 is None or numero2 is None:
+        return "Faltan datos"
+    return f"El resultado de la suma es {sumar(numero1,numero2)}"
+
+@app.route("/division_piso")
+def ruta_division_piso():
+    numero1=request.args.get("numero1",type=float)
+    numero2=request.args.get("numero2",type=float)
+    if numero1 is None or numero2 is None:
+        return "Faltan datos"
+    return f"El resultado de la suma es {sumar(numero1,numero2)}"
+
+
+
+if __name__ == "__main__":
+    server = Server(app.wsgi_app)
+    server.serve()
--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-from flask import Flask
+from flask import Flask, request
 
 app=Flask(__name__)
+
+def suma(a,b):
+    return a+b
 
 @app.route("/")
 def home ():
     return '''
         Aplicación web con flask-afsr
-        <h1>Calcu1adora</h1>
+        <h1>Calculadora</h1>
         <h3>Opciones Disponibles</h3>
         <ul>
-        <li><a href="
+            <li><a href="/Suma">Suma</a></li>
+            <li>Resta</li>
+            <li>Multiplicacion</li>
+            <li>Division</li>
         
-
-
-
+        
+        
+        </ul>
 
 
 '''
+@app.route("/suma")
+def ruta_suma():
+    print(request.args)
+    num1 = request.arg.get("num1",type=float)
+    num2 = request.arg.get("num2", type=float)
+    if num1 is None or num2 is None:
+        return "Faltan parametros"
+    return f"<p>La suma de {num1} + {num2} es {suma(num1,num2)}</p>"
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
 
